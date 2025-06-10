6671da781c6260428045651bec88ef0649f8cc0c
Junior <68521496+jrftw@users.noreply.github.com>
2025-06-10 01:04:30 -0400

    archive error

--- Infinitum Block Smash/Infinitum Block Smash/Infinitum_Block_SmashApp.swift
+++ Infinitum Block Smash/Infinitum Block Smash/Infinitum_Block_SmashApp.swift
@@ -14,6 +14,7 @@ import BackgroundTasks
 import GameKit
 import FirebaseDatabase
 import FirebaseAnalytics
+import DeviceCheck
 
 // MARK: - AppDelegate
 class AppDelegate: NSObject, UIApplicationDelegate {
